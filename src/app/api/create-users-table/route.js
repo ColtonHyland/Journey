4f840653d6fb96f28af6388a3b2bcc4227f1c90e
Colton Hyland <colthyland@gmail.com>
2023-07-20 10:07:36 -0700

    Create users folder

---
+++ src/app/api/create-users-table/route.js
@@ -0,0 +1,20 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+
+export default async function createUsersTable(request) {
+  try {
+    const result = await sql`
+      CREATE TABLE users (
+        user_id SERIAL PRIMARY KEY,
+        username VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL,
+        password VARCHAR(100) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
