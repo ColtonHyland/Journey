c6b79f5804be4943a89a47baba25e28654a3e268
Colton Hyland <colthyland@gmail.com>
2023-07-25 18:35:59 -0700

    Prisma db migrations

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,73 @@
+-- CreateTable
+CREATE TABLE "calendar_events" (
+    "event_id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "title" VARCHAR(100) NOT NULL,
+    "description" TEXT,
+    "event_datetime" TIMESTAMP(6),
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "calendar_events_pkey" PRIMARY KEY ("event_id")
+);
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "name" VARCHAR(255),
+    "owner" VARCHAR(255)
+);
+
+-- CreateTable
+CREATE TABLE "tasks" (
+    "task_id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "task_type" VARCHAR(20) NOT NULL,
+    "title" VARCHAR(100) NOT NULL,
+    "description" TEXT,
+    "due_date" DATE,
+    "status" VARCHAR(20),
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "timeframe_id" INTEGER,
+    "journal_entry" TEXT,
+
+    CONSTRAINT "tasks_pkey" PRIMARY KEY ("task_id")
+);
+
+-- CreateTable
+CREATE TABLE "timeframes" (
+    "timeframe_id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "timeframe_type" VARCHAR(10) NOT NULL,
+    "start_date" DATE,
+    "end_date" DATE,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "timeframes_pkey" PRIMARY KEY ("timeframe_id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "user_id" SERIAL NOT NULL,
+    "username" VARCHAR(100) NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("user_id")
+);
+
+-- AddForeignKey
+ALTER TABLE "calendar_events" ADD CONSTRAINT "calendar_events_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_timeframe_id_fkey" FOREIGN KEY ("timeframe_id") REFERENCES "timeframes"("timeframe_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "timeframes" ADD CONSTRAINT "timeframes_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
