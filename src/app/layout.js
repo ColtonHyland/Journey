b0c4229c839cc6cb14e7602f8df2d2de4ea85207
Colton Hyland <colthyland@gmail.com>
2023-08-28 15:27:11 -0700

    error testing with vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,12 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      {/* <body className={inter.className}>
         <Providers>
           <Appbar />
           {children}
         </Providers>
-      </body>
+      </body> */}
     </html>
   )
 }
