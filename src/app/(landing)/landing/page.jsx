8a552fda28f859298bace0fa13b6583be5c1ebd1
Colton Hyland <colthyland@gmail.com>
2023-08-29 16:10:50 -0700

    Landing page template

--- src/app/(landing)/landing/page.jsx
+++ src/app/(landing)/landing/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const LandingPage = () => {
   return (
-    <div>Landing Page</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-semibold mb-4">Welcome to Your App</h1>
+      <p className="text-gray-600 text-lg mb-8">Start managing your tasks and goals with ease.</p>
+      <button className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600">
+        Get Started
+      </button>
+    </div>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
--- src/app/(landing)/page.js
+++ src/app/(landing)/page.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
-  )
-}
+    <div className="bg-gradient-to-b from-blue-200 to-blue-400 h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold text-white mb-4">
+        Embark on a transformative experience with Journey.
+      </h1>
+      <p className="text-lg text-white text-center">
+        Seamlessly manage your goals, tasks, and reflections in one place,
+        empowering you to make the most of each step in your personal and professional journey.
+      </p>
+    </div>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
