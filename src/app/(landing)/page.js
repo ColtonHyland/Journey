c749b8ca659977dedb91bc134d0b7e4f0b084be3
Colton Hyland <colthyland@gmail.com>
2023-09-04 02:07:16 -0700

    Reorganised pages

--- src/app/(landing)/page.js
+++ src/app/(landing)/page.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 const LandingPage = () => {
   return (
@@ -6,7 +7,7 @@ const LandingPage = () => {
       <h1 className="text-4xl font-bold text-white mb-4">
         Embark on a transformative experience with Journey.
       </h1>
-      <p className="text-lg text-white text-center">
+      <p className="text-lg text-white text-center mb-6">
         Seamlessly manage your goals, tasks, and reflections in one place,
         empowering you to make the most of each step in your personal and professional journey.
       </p>
