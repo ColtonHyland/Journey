4260d0f6f399c85feccc8b54d5519a829eef314e
Colton Hyland <colthyland@gmail.com>
2023-09-29 12:05:55 -0700

    Table is populated. NExt is API endpoints

--- src/app/api/add-user/route.js
+++ src/app/api/add-user/route.js
@@ -1,59 +1,26 @@
-import { sql } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
+const { PrismaClient } = require('@prisma/client')
 
-export async function GET(request) {
-  const { searchParams } = new URL(request.url);
-  const username = searchParams.get('username');
-  const email = searchParams.get('email');
-  const password = searchParams.get('password');
+const prisma = new PrismaClient({ log: ['query']})
 
-  try {
-    if (!username || !email || !password) {
-      throw new Error('Username, email, and password are required');
-    }
-
-    // Import Prisma Client
-const { PrismaClient } = require('@prisma/client');
-
-// Create an instance of Prisma Client
-const prisma = new PrismaClient();
-
-// Function to create a user
-async function createUser(username, email, password) {
-  try {
-    const user = await prisma.users.create({
-      data: {
-        username,
-        email,
-        password,
-      },
-    });
-
-    console.log('User created:', user);
-    return user;
-  } catch (error) {
-    console.error('Error creating user:', error);
-    throw error;
-  } finally {
-    // Disconnect Prisma Client
-    await prisma.$disconnect();
-  }
-}
-
-// Example usage
 async function main() {
-  const newUser = await createUser('john_doe', 'john@example.com', 'password123');
-}
+  
+  await prisma.users.deleteMany();
+  const user = await prisma.users.create({
+    data: {
+      username: 'Colton Hyland',
+      email: 'colthyland@gmail.com',
+      password: 'password123',
+    },
+  })
+  console.log(user)
+};
 
-// Call the main function to create the user
-main().catch((e) => {
-  throw e;
-});
-
-    return NextResponse.json({ users }, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-}
-
-//http://localhost:3001/api/add-user?username=coltonhyland&email=colthyand@gmail.com&password=123
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
