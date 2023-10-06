4b8377244c853f4b2475bd4d424ce7629ed23222
Colton Hyland <colthyland@gmail.com>
2023-10-05 17:26:23 -0700

    SignIn sets email to cookie

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth/next"
 import GoogleProvider from 'next-auth/providers/google'
 import saveUserToDatabase from "../../saveUserToDatabase/route.js";
 import getUserByEmail from "../../getUserByEmail/route.js";
+import { setCookie } from 'cookies';
 
 const handler = NextAuth({
   providers: [
@@ -35,6 +36,14 @@ const handler = NextAuth({
         console.log(`User with email ${email} already exists.`);
       }
   
+      // Assuming you have the user's email in a variable called 'userEmail'
+      setCookie(null, 'email', email, {
+        maxAge: 30 * 24 * 60 * 60, // 30 days (adjust as needed)
+        path: '/', // Cookie is available on all routes
+        secure: process.env.NODE_ENV === 'production', // Set to true in production for HTTPS
+        sameSite: 'lax', // Adjust this as needed for your use case
+      });
+
       return true; // Continue with the sign-in process
     },
   },
