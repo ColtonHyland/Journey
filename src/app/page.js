8232ab3a79c7c32aa2f9d86678db9b571fa13081
Colton Hyland <colthyland@gmail.com>
2023-10-06 19:50:34 -0700

    Loading bar added between login time

--- src/app/page.js
+++ src/app/page.js
@@ -5,29 +5,37 @@ import Link from 'next/link';
 import { useSession } from 'next-auth/react'; // Import useSession from NextAuth
 
 const LandingPage = () => {
-  const { data: session } = useSession(); // Get the user's session data
+  
+  const { data: session, status } = useSession();
+  const isLoading = status === 'loading';
 
   return (
     <div className="bg-gradient-to-b from-blue-200 to-blue-400 h-screen flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold text-white mb-4">
-        {session ? `Welcome, ${session.user.name}!` : 'Embark on a transformative experience with Journey.'}
-      </h1>
-      <p className="text-lg text-white text-center mb-6">
-        {session
-          ? 'You are now logged in. Start managing your goals, tasks, and reflections.'
-          : 'Seamlessly manage your goals, tasks, and reflections in one place, empowering you to make the most of each step in your personal and professional journey.'}
-      </p>
-      <Link href="/dashboard" passHref>
-        {session ? (
-          <button className="text-lg text-white bg-green-600 hover:bg-green-700 py-2 px-4 rounded">
-            Go to Dashboard
-          </button>
-        ) : (
-          <button className="text-lg text-white bg-green-600 hover:bg-green-700 py-2 px-4 rounded">
-            Get Started
-          </button>
-        )}
-      </Link>
+      {isLoading ? (
+        <div className="text-4xl font-bold text-white mb-4">Loading...</div>
+      ) : (
+        <>
+          <h1 className="text-4xl font-bold text-white mb-4">
+            {session ? `Welcome, ${session.user.name}!` : 'Embark on a transformative experience with Journey.'}
+          </h1>
+          <p className="text-lg text-white text-center mb-6">
+            {session
+              ? 'You are now logged in. Start managing your goals, tasks, and reflections.'
+              : 'Seamlessly manage your goals, tasks, and reflections in one place, empowering you to make the most of each step in your personal and professional journey.'}
+          </p>
+          <Link href="/dashboard" passHref>
+            {session ? (
+              <button className="text-lg text-white bg-green-600 hover-bg-green-700 py-2 px-4 rounded">
+                Go to Dashboard
+              </button>
+            ) : (
+              <button className="text-lg text-white bg-green-600 hover-bg-green-700 py-2 px-4 rounded">
+                Get Started
+              </button>
+            )}
+          </Link>
+        </>
+      )}
     </div>
   );
 };
