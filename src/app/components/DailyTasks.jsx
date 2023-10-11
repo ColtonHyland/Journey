d27f0ca237c791bd669ccfcbfff378aacb18b22b
Colton Hyland <colthyland@gmail.com>
2023-10-10 21:08:07 -0700

    Daily Tasks component uses fetch route

--- src/app/components/DailyTasks.jsx
+++ src/app/components/DailyTasks.jsx
@@ -1,23 +1,40 @@
-// components/DailyTasks.js
+"use client"
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const DailyTasks = ({ tasks }) => {
+const DailyTasks = () => {
+  const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch daily tasks when the component mounts
+    fetch('/api/get-daily-tasks') // Replace with the actual API route
+      .then((response) => response.json())
+      .then((data) => {
+        setTasks(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching daily tasks:', error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
       <h2 className="text-2xl font-semibold mb-4">Daily Tasks</h2>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.task_id} className="mb-2">
-            <input
-              type="checkbox"
-              className="mr-2"
-              // Handle the task completion logic here
-            />
-            {task.title}
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <p>Loading tasks...</p>
+      ) : (
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.task_id} className="mb-2">
+              <input type="checkbox" className="mr-2" />
+              {task.title}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
