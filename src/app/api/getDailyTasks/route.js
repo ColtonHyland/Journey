5eac42d892ac0d1e4e1eac320d35f8469745093b
Colton Hyland <colthyland@gmail.com>
2023-10-13 13:53:37 -0700

    Route handlers swapped for Next convention. Prisma was being accesseaccessed form the client (browser) and not the server side.

--- src/app/api/getDailyTasks/route.js
+++ src/app/api/getDailyTasks/route.js
@@ -2,25 +2,52 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function getDailyTasks(user_id) {
-  console.log('getDailyTasks for user_id:', user_id);
-  try {
-    const tasks = await prisma.tasks_and_goals.findMany({
-      where: {
-        user_id: user_id,
-        type: 'task', // Assuming daily tasks have the type 'task'
-        // You can add more conditions or filters here if needed
-      },
-    });
-    return tasks;
-  } catch (error) {
-    throw new Error(`Error fetching daily tasks: ${error.message}`);
-  } finally {
-    await prisma.$disconnect();
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    try {
+      const tasks = await prisma.tasks_and_goals.findMany({
+        where: {
+          user_id: 1, // Replace with the user's ID or obtain it securely
+          type: 'task', // Replace with the appropriate task type
+        },
+      });
+
+      res.status(200).json(tasks);
+    } catch (error) {
+      console.error('Error fetching daily tasks:', error);
+      res.status(500).json({ error: 'Error fetching tasks' });
+    } finally {
+      await prisma.$disconnect();
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
   }
 }
 
-export default getDailyTasks;
+// import { PrismaClient } from '@prisma/client';
+
+// const prisma = new PrismaClient();
+
+// async function getDailyTasks(user_id) {
+//   'use server';
+//   console.log('getDailyTasks for user_id:', user_id);
+//   try {
+//     const tasks = await prisma.tasks_and_goals.findMany({
+//       where: {
+//         user_id: user_id,
+//         type: 'task', // Assuming daily tasks have the type 'task'
+//         // You can add more conditions or filters here if needed
+//       },
+//     });
+//     return tasks;
+//   } catch (error) {
+//     throw new Error(`Error fetching daily tasks: ${error.message}`);
+//   } finally {
+//     await prisma.$disconnect();
+//   }
+// }
+
+// export default getDailyTasks;
 
 // const { data: session } = useSession();
 //   const router = useRouter();
--- src/app/api/getUserByEmail/route.js
+++ src/app/api/getUserByEmail/route.js
@@ -1,21 +1,52 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function getUserByEmail(email) {
-  console.log("getUserByEmail: ", email)
-  try {
-    const user = await prisma.users.findUnique({
-      where: {
-        email: email,
-      },
-    });
-    return user;
-  } catch (error) {
-    throw new Error(`Error fetching user by email: ${error.message}`);
-  } finally {
-    await prisma.$disconnect();
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const { email } = req.body; // Assuming you send the email in the request body
+      const user = await prisma.users.findUnique({
+        where: {
+          email: email,
+        },
+      });
+
+      if (!user) {
+        res.status(404).json({ error: 'User not found' });
+      } else {
+        res.status(200).json(user);
+      }
+    } catch (error) {
+      console.error('Error fetching user by email:', error);
+      res.status(500).json({ error: 'Error fetching user' });
+    } finally {
+      await prisma.$disconnect();
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
   }
 }
 
-export default getUserByEmail;
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// async function getUserByEmail(email) {
+//   'use server';
+//   console.log("getUserByEmail: ", email)
+//   try {
+//     const user = await prisma.users.findUnique({
+//       where: {
+//         email: email,
+//       },
+//     });
+//     return user;
+//   } catch (error) {
+//     throw new Error(`Error fetching user by email: ${error.message}`);
+//   } finally {
+//     await prisma.$disconnect();
+//   }
+// }
+
+// export default getUserByEmail;
