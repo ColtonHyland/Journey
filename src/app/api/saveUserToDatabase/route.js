f0909fdd86ee0c1ee7012f34ee4c0e946debff05
Colton Hyland <colthyland@gmail.com>
2023-10-26 12:04:12 -0700

    Updated saveUser route

--- src/app/api/saveUserToDatabase/route.js
+++ src/app/api/saveUserToDatabase/route.js
@@ -1,27 +1,66 @@
 import { PrismaClient } from "@prisma/client";
+import { NextResponse } from 'next/server';
 
-export default async function handler(req, res) {
-  
+export async function POST(request) {
   const prisma = new PrismaClient();
-  
-  if (req.method === "POST") {
+
+  if (request.method === "POST") {
     try {
-      const userData = req.body; // Assuming you send the user data in the request body
+      const userData = await request.json(); // Assuming you send the user data in the request body
 
       const user = await prisma.users.create({
         data: userData,
       });
 
-      res.status(201).json(user); // Use a 201 status code for successful creation
+      return NextResponse.json({
+        message: 'User created',
+        user,
+      }, {
+        status: 201, // Use a 201 status code for successful creation
+      });
     } catch (error) {
       console.error("Error creating a new user:", error);
-      res.status(500).json({ error: "Error creating a new user" });
+      return NextResponse.json({
+        message: 'Error creating a new user',
+      }, {
+        status: 500,
+      });
+    } finally {
+      await prisma.$disconnect();
     }
   } else {
-    // 405 Method Not Allowed
-    res.status(405).json({ message: "Method not allowed" });
+    return NextResponse.json({
+      message: 'Method not allowed',
+    }, {
+      status: 405, // 405 Method Not Allowed
+    });
   }
 }
+
+// import { PrismaClient } from "@prisma/client";
+
+// export default async function handler(req, res) {
+  
+//   const prisma = new PrismaClient();
+  
+//   if (req.method === "POST") {
+//     try {
+//       const userData = req.body; // Assuming you send the user data in the request body
+
+//       const user = await prisma.users.create({
+//         data: userData,
+//       });
+
+//       res.status(201).json(user); // Use a 201 status code for successful creation
+//     } catch (error) {
+//       console.error("Error creating a new user:", error);
+//       res.status(500).json({ error: "Error creating a new user" });
+//     }
+//   } else {
+//     // 405 Method Not Allowed
+//     res.status(405).json({ message: "Method not allowed" });
+//   }
+// }
 
 // import { PrismaClient } from "@prisma/client";
 
