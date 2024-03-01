c17fea1bfe36efe3a53ca4a39b8a0b91ca267f50
Colton - Work <colthyland@gmail.com>
2024-03-01 12:44:01 -0800

    Update route structure to Next current version

--- src/app/api/addDailyTask/route.js
+++ src/app/api/addDailyTask/route.js
@@ -1,29 +1,43 @@
+import { NextResponse } from "next/server";
 import { prisma } from "../../lib/prisma"; // Adjust the import path based on your project structure
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { title, userId } = req.body;
+export default async function POST(request) {
+  const res = await request.json();
+  const { title, userId } = res;
+  const result = await prisma.tasks_and_goals.create({
+    data: {
+      title,
+      userId,
+      type: "task", // Assuming type is required and set to "task" for daily tasks
+      // Add other fields as necessary, setting default values or nulls
+    },
+  });
+  return NextResponse.json({result});
 
-    try {
-      const newTask = await prisma.tasks_and_goals.create({
-        data: {
-          title,
-          userId,
-          type: "task", // Assuming type is required and set to "task" for daily tasks
-          // Add other fields as necessary, setting default values or nulls
-        },
-      });
 
-      return res.status(200).json(newTask);
-    } catch (error) {
-      console.error("Failed to add task:", error);
-      return res.status(500).json({ error: "Failed to add task" });
-    }
-  } else {
-    // Handle any requests that aren't POST
-    res.setHeader('Allow', ['POST']);
-    return res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+  // if (req.method === 'POST') {
+  //   const { title, userId } = req.body;
+
+  //   try {
+  //     const newTask = await prisma.tasks_and_goals.create({
+  //       data: {
+  //         title,
+  //         userId,
+  //         type: "task", // Assuming type is required and set to "task" for daily tasks
+  //         // Add other fields as necessary, setting default values or nulls
+  //       },
+  //     });
+
+  //     return res.status(200).json(newTask);
+  //   } catch (error) {
+  //     console.error("Failed to add task:", error);
+  //     return res.status(500).json({ error: "Failed to add task" });
+  //   }
+  // } else {
+  //   // Handle any requests that aren't POST
+  //   res.setHeader('Allow', ['POST']);
+  //   return res.status(405).end(`Method ${req.method} Not Allowed`);
+  // }
 }
 
 
