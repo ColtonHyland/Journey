312c8ce01d36ff843ab42d9ed7bc613f0766efb4
Colton - Work <colthyland@gmail.com>
2024-03-30 08:58:19 -0700

    day shedule page

---
+++ src/app/(calendar)/schedule/[date]/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SchedulePage = ({ date }) => {
+  return (
+    <div>
+      <h1>Schedule for {date}</h1>
+      <p>This is a placeholder page for schedules on a specific date.</p>
+    </div>
+  );
+};
+
+export default SchedulePage;
