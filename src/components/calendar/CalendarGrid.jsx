6917dabc9530a43d56a2cc242e9900ec14fd5b16
Colton - Work <colthyland@gmail.com>
2024-03-30 09:07:34 -0700

    Day schedule page linked from calendar

--- src/components/calendar/CalendarGrid.jsx
+++ src/components/calendar/CalendarGrid.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { useCalendar } from './CalendarContext';
 import { startOfMonth, endOfMonth, eachDayOfInterval, format } from 'date-fns';
 
@@ -13,7 +14,11 @@ const CalendarGrid = () => {
     <div className="calendar-grid">
       {days.map(day => (
         <div key={day} className="day">
-          {format(day, 'd')}
+          <Link href={`/schedule/${format(day, 'yyyy-MM-dd')}`}>
+            {format(day, 'd')}
+          </Link>
+          {/* {format(day, 'd')} */}
+          {/* /schedule/${format(day, 'yyyy-MM-dd')} */}
           {/* Additional task display logic goes here */}
         </div>
       ))}
