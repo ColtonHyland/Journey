cdaff9411da3c6817fedca4c0a16410128869e32
Colton - Work <colthyland@gmail.com>
2024-04-09 12:42:43 -0700

    swapped landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,13 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import Link from 'next/link';
-import { useSession } from 'next-auth/react'; // Import useSession from NextAuth
-import { useRouter, redirect } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 import LoadingSpinner from '../components/LoadingSpinner';
 
 const LandingPage = () => {
   const { data: session, status } = useSession();
-  const router = useRouter();
   const isLoading = status === 'loading';
-
-  useEffect(() => {
-    if (!isLoading && session) {
-      router.replace('/dashboard');
-    }
-  }, [isLoading, session, router]);
 
   return (
     <div className="bg-gradient-to-b from-blue-200 to-blue-400 h-screen flex flex-col justify-center items-center">
@@ -33,7 +25,7 @@ const LandingPage = () => {
               ? 'You are now logged in. Start managing your goals, tasks, and reflections.'
               : 'Seamlessly manage your goals, tasks, and reflections in one place, empowering you to make the most of each step in your personal and professional journey.'}
           </p>
-          <Link href={session ? "/dashboard" : "/login"} passHref> {/* Adjusted link based on session */}
+          <Link href={session ? "/dashboard" : "/login"} passHref>
             <button className="text-lg text-white bg-green-600 hover-bg-green-700 py-2 px-4 rounded">
               {session ? 'Go to Dashboard' : 'Get Started'}
             </button>
