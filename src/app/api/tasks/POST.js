e15445aa31f7e94f8f869e5bb135316370847164
Colton - Work <colthyland@gmail.com>
2024-05-03 15:13:12 -0700

    Repead interval in post

--- src/app/api/tasks/POST.js
+++ src/app/api/tasks/POST.js
@@ -2,28 +2,48 @@ import prisma from '@/lib/prisma';
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth/next";
 import { NextResponse } from 'next/server';
+import { addDays, eachDayOfInterval, parseISO, isMatch } from 'date-fns';
 
 export async function POST(request) {
-
   const session = await getServerSession(authOptions);
   if (!session) {
     return new Response(JSON.stringify({ error: "Unauthorized" }), { status: 401 });
   }
   const userId = session.user.id;
-  const { title, description, goalId, assigned_date, start_time, end_time} = await request.json();
+  const { title, description, goalId, assigned_date, start_time, end_time, repeatUntil, repeatDays } = await request.json();
 
   try {
-
-    const task = await prisma.task.create({
-      data: {
+    let tasksToCreate = [];
+    if (repeatUntil && repeatDays) {
+      const startDate = parseISO(assigned_date);
+      const endDate = parseISO(repeatUntil);
+      eachDayOfInterval({ start: startDate, end: endDate }).forEach(day => {
+        if (repeatDays[day.getDay()]) { // Assuming repeatDays is an array reflecting days from 0 (Sunday) to 6 (Saturday)
+          tasksToCreate.push({
+            title,
+            description,
+            userId: session.user.id,
+            assigned_date: day,
+            start_time: new Date(`${day.toISOString().split('T')[0]}T${start_time}`),
+            end_time: new Date(`${day.toISOString().split('T')[0]}T${end_time}`),
+          });
+        }
+      });
+    } else {
+      tasksToCreate.push({
         title,
         description,
         userId: session.user.id,
         assigned_date: new Date(assigned_date),
         start_time: new Date(start_time),
         end_time: new Date(end_time),
-      },
-    });
+      });
+    }
+
+    // Batch create tasks
+    for (const taskData of tasksToCreate) {
+      await prisma.task.create({ data: taskData });
+    }
 
     return new NextResponse(JSON.stringify({ task }), {
       status: 201,
