92059a9b1baa9a6d27c6002e9d826610269bb12b
Colton - Work <colthyland@gmail.com>
2024-05-16 11:16:57 -0700

    styling changes for diaglog box

--- src/components/calendar/date/ConfirmationDialog.jsx
+++ src/components/calendar/date/ConfirmationDialog.jsx
@@ -4,13 +4,19 @@ import React from "react";
 const ConfirmationDialog = ({ message, onConfirm, onCancel }) => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white rounded-lg shadow-lg p-6 w-96 relative">
+      <div className="bg-white rounded-lg shadow-lg p-6 relative">
         <p>{message}</p>
-        <div className="flex justify-end mt-4">
-          <button onClick={onCancel} className="mr-2 px-4 py-2 bg-gray-200 rounded">
+        <div className="flex justify-center space-x-4 mt-4">
+          <button
+            onClick={onCancel}
+            className="py-2 px-4 border-2 border-black shadow-sm text-sm font-medium rounded-md text-black bg-white hover:bg-gray-100"
+          >
             Cancel
           </button>
-          <button onClick={onConfirm} className="px-4 py-2 bg-blue-500 text-white rounded">
+          <button
+            onClick={onConfirm}
+            className="py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-black hover:bg-gray-900"
+          >
             Confirm
           </button>
         </div>
